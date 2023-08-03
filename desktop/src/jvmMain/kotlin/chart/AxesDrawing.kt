80e328af29e0c183b68512644dc54f572f7fbd49
AndrewAboalhana <hana_hany6@yahoo.com>
2023-08-03 08:12:27 +0300

    fix bug line go out of boundaries

--- desktop/src/jvmMain/kotlin/chart/AxesDrawing.kt
+++ desktop/src/jvmMain/kotlin/chart/AxesDrawing.kt
@@ -28,7 +28,6 @@ fun AxesDrawing(
     yAxisLabel: String = ChartDefault.chart.yAxisLabel,
     xAxisData: List<String> = ChartDefault.chart.xAxisData,
 ) {
-
     val spacing = 130f
     val upperValue = remember {
         linesParameters.flatMap { it.data }.maxOrNull()?.plus(1.0) ?: 0.0
@@ -43,10 +42,10 @@ fun AxesDrawing(
     val textMeasure = rememberTextMeasurer()
 
 
+
     Canvas(modifier = modifier) {
         val spaceBetweenXes = (size.width - spacing) / (linesParameters[0].data.size - 1)
         val barWidthPx = 0.2.dp.toPx()
-
 
         xAxisData.forEachIndexed { index, dataPoint ->
             val xLength = spacing + index * spaceBetweenXes
@@ -61,136 +60,159 @@ fun AxesDrawing(
                     topLeft = Offset(xLength, size.height / 1.07f)
                 )
             }
+        }
 
+        // Calculate the valid boundaries of the chart area
+        val minX = spacing
+        val maxX = size.width - 55
+        val minY = spacing
+        val maxY = size.height - spacing
 
-            // for y coordinate
-            val priceRange = upperValue - lowerValue
-            val priceStep = priceRange / 5f
+        // Draw background lines
+        (0..4).forEach { i ->
+            yAxis.add(size.height - spacing - i * size.height / 8f)
+            val y = yAxis[i] + 12f
 
-            (0..4).forEach { i ->
-                drawContext.canvas.nativeCanvas.apply {
-                    val yValue = lowerValue + priceStep * i
+            // Ensure the line stays within the boundaries
+            val xStart = (spacing - 10).coerceIn(minX, maxX)
+            val xEnd = (size.width).coerceIn(minX, maxX)
 
-                    drawText(
-                        textMeasurer = textMeasure, text = yValue.toInt().toString(),
-                        style = TextStyle(
-                            fontSize = 12.sp,
-                            color = Color.Gray,
-                        ),
-                        topLeft = Offset(0f, size.height - spacing - i * size.height / 8f)
-                    )
-                }
-            }
+            drawLine(
+                backGroundColor,
+                start = Offset(xStart, y),
+                end = Offset(xEnd, y),
+                strokeWidth = barWidthPx,
+                pathEffect = pathEffect
+            )
+        }
 
+        // Draw y-axis labels
+        val priceRange = upperValue - lowerValue
+        val priceStep = priceRange / 5f
+        (0..4).forEach { i ->
+            val yValue = lowerValue + priceStep * i
+            val y = (size.height - spacing - i * size.height / 8f).coerceIn(minY, maxY)
 
-            //background line
-            (0..4).forEach { i ->
-                yAxis.add(size.height - spacing - i * size.height / 8f)
-                drawLine(
-                    backGroundColor,
-                    start = Offset(spacing - 10, yAxis[i] + 12f),
-                    end = Offset(xLength + 55, yAxis[i] + 12f),
-                    strokeWidth = barWidthPx,
-                    pathEffect = pathEffect
+            drawContext.canvas.nativeCanvas.apply {
+                drawText(
+                    textMeasurer = textMeasure, text = yValue.toInt().toString(),
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        color = Color.Gray,
+                    ),
+                    topLeft = Offset(0f, y)
                 )
             }
+        }
 
+        // Lines drawing
+        linesParameters.forEach { line ->
+            if (line.lineType == LineType.DEFAULT_LINE) {
+                val strokePathDefault = Path().apply {
+                    val height = size.height
+                    line.data.indices.forEach { i ->
+                        val info = line.data[i]
+                        val ratio = (info.toFloat() - lowerValue) / (upperValue - lowerValue)
 
-            // lines drawing
-            linesParameters.forEach { line ->
-                if (line.lineType == LineType.DEFAULT_LINE) {
-                    val strokePathDefault = Path().apply {
-                        val height = size.height
-                        line.data.indices.forEach { i ->
-                            val info = line.data[i]
-                            val ratio = (info.toFloat() - lowerValue) / (upperValue - lowerValue)
+                        val x1 = spacing + i * spaceBetweenXes
+                        val y1 = height - spacing - (ratio * height).toFloat()
 
-                            val x1 = spacing + i * spaceBetweenXes
-                            val y1 = height - spacing - (ratio * height).toFloat()
+                        // Adjust the coordinates to stay within boundaries
+                        val xAdjusted = x1.coerceIn(minX, maxX)
+                        val yAdjusted = y1.coerceIn(minY, maxY)
 
-                            if (i == 0) {
-                                moveTo(x1, y1)
-                            }
-                            lineTo(x1, y1)
+                        if (i == 0) {
+                            moveTo(xAdjusted, yAdjusted)
+                        } else {
+                            lineTo(xAdjusted, yAdjusted)
                         }
+                    }
+                }
+
+                drawPath(
+                    path = strokePathDefault,
+                    color = line.lineColor,
+                    style = Stroke(
+                        width = 3.dp.toPx(),
+                        cap = StrokeCap.Round
+                    )
+                )
+
+                if (line.lineShadow == LineShadow.SHADOW) {
+                    val fillPath = strokePathDefault.apply {
+                        lineTo(size.width - spaceBetweenXes, size.height - spacing)
+                        lineTo(spacing, size.height - spacing)
+                        close()
                     }
 
                     drawPath(
-                        path = strokePathDefault,
-                        color = line.lineColor,
-                        style = Stroke(
-                            width = 3.dp.toPx(),
-                            cap = StrokeCap.Round
+                        path = fillPath,
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                line.lineColor.copy(alpha = .3f),
+                                Color.Transparent
+                            ),
+                            endY = size.height - spacing
                         )
                     )
-                    if (line.lineShadow == LineShadow.SHADOW) {
-                        val fillPath = strokePathDefault.apply {
-                            lineTo(size.width - spaceBetweenXes, size.height - spacing)
-                            lineTo(spacing, size.height - spacing)
-                            close()
-                        }
+                }
+            } else {
+                var medX: Float
+                var medY: Float
+                val strokePath = Path().apply {
+                    val height = size.height
+                    line.data.indices.forEach { i ->
+                        val nextInfo = line.data.getOrNull(i + 1) ?: line.data.last()
+                        val firstRatio = (line.data[i].toFloat() - lowerValue) / (upperValue - lowerValue)
+                        val secondRatio = (nextInfo.toFloat() - lowerValue) / (upperValue - lowerValue)
 
-                        drawPath(
-                            path = fillPath,
-                            brush = Brush.verticalGradient(
-                                colors = listOf(
-                                    line.lineColor.copy(alpha = .3f),
-                                    Color.Transparent
-                                ),
-                                endY = size.height - spacing
-                            )
-                        )
-                    }
-                } else {
-                    var medX: Float
-                    var medY: Float
-                    val strokePath = Path().apply {
-                        val height = size.height
-                        line.data.indices.forEach { i ->
-                            val nextInfo = line.data.getOrNull(i + 1) ?: line.data.last()
-                            val firstRatio = (line.data[i].toFloat() - lowerValue) / (upperValue - lowerValue)
-                            val secondRatio = (nextInfo.toFloat() - lowerValue) / (upperValue - lowerValue)
+                        val x1 = spacing + i * spaceBetweenXes
+                        val y1 = height - spacing - (firstRatio * height).toFloat()
+                        val x2 = spacing + (i + 1) * spaceBetweenXes
+                        val y2 = height - spacing - (secondRatio * height).toFloat()
 
-                            val x1 = spacing + i * spaceBetweenXes
-                            val y1 = height - spacing - (firstRatio * height).toFloat()
-                            val x2 = spacing + (i + 1) * spaceBetweenXes
-                            val y2 = height - spacing - (secondRatio * height).toFloat()
-                            if (i == 0) {
-                                moveTo(x1, y1)
-                            } else {
-                                medX = (x1 + x2) / 2f
-                                medY = (y1 + y2) / 2f
-                                quadraticBezierTo(x1 = x1, y1 = y1, x2 = medX, y2 = medY)
-                            }
+                        // Adjust the coordinates to stay within boundaries
+                        val x1Adjusted = x1.coerceIn(minX, maxX)
+                        val y1Adjusted = y1.coerceIn(minY, maxY)
+                        val x2Adjusted = x2.coerceIn(minX, maxX)
+                        val y2Adjusted = y2.coerceIn(minY, maxY)
+
+                        if (i == 0) {
+                            moveTo(x1Adjusted, y1Adjusted)
+                        } else {
+                            medX = (x1Adjusted + x2Adjusted) / 2f
+                            medY = (y1Adjusted + y2Adjusted) / 2f
+                            quadraticBezierTo(x1Adjusted, y1Adjusted, medX, medY)
                         }
                     }
+                }
+
+                drawPath(
+                    path = strokePath,
+                    color = line.lineColor,
+                    style = Stroke(
+                        width = 3.dp.toPx(),
+                        cap = StrokeCap.Round
+                    )
+                )
+
+                if (line.lineShadow == LineShadow.SHADOW) {
+                    val fillPath = strokePath.apply {
+                        lineTo(size.width - spaceBetweenXes, size.height - spacing)
+                        lineTo(spacing, size.height - spacing)
+                        close()
+                    }
+
                     drawPath(
-                        path = strokePath,
-                        color = line.lineColor,
-                        style = Stroke(
-                            width = 3.dp.toPx(),
-                            cap = StrokeCap.Round
+                        path = fillPath,
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                line.lineColor.copy(alpha = .3f),
+                                Color.Transparent
+                            ),
+                            endY = size.height - spacing
                         )
                     )
-
-                    if (line.lineShadow == LineShadow.SHADOW) {
-                        val fillPath = strokePath.apply {
-                            lineTo(size.width - spaceBetweenXes, size.height - spacing)
-                            lineTo(spacing, size.height - spacing)
-                            close()
-                        }
-
-                        drawPath(
-                            path = fillPath,
-                            brush = Brush.verticalGradient(
-                                colors = listOf(
-                                    line.lineColor.copy(alpha = .3f),
-                                    Color.Transparent
-                                ),
-                                endY = size.height - spacing
-                            )
-                        )
-                    }
                 }
             }
         }
