5932c818f5068e1008ae1dfc94027dad8922f284
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2023-08-06 17:03:59 +0300

    add composables we need custom text icon and dropdown

---
+++ chart/src/main/kotlin/composables/Text.kt
@@ -0,0 +1,38 @@
+package com.example.comcus.composables
+
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+
+@Composable
+fun CustomText(
+    text: String,
+    modifier: Modifier = Modifier,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = TextStyle.Default,
+    color: Color = Color.Unspecified,
+    ){
+    val textColor = color.takeOrElse {
+        style.color.takeOrElse {
+            LocalContentColor.current
+        }
+    }
+    BasicText(
+        text,
+        modifier,
+        style.copy(color = textColor),
+        onTextLayout,
+        overflow,
+        softWrap,
+        maxLines,
+    )
+
+}
