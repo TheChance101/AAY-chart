8cd4bf389068522ebcde453757d40d8dcb3a8c47
amnah.a <amnah44ali@gmail.com>
2023-08-07 02:05:20 +0300

    convert chart project to module

--- chart/build.gradle.kts
+++ chart/build.gradle.kts
@@ -8,11 +8,6 @@ plugins {
 group = "com.example"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
-}
 
 dependencies {
     // Note, if you develop a library, you should use compose.desktop.common.
@@ -20,16 +15,4 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
-}
-
-compose.desktop {
-    application {
-        mainClass = "MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "chart"
-            packageVersion = "1.0.0"
-        }
-    }
 }
