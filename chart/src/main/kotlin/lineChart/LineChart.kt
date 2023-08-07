e7fdd2824c3fcf2ac3d20cab91c697e175b61ece
amnah.a <amnah44ali@gmail.com>
2023-08-07 20:33:11 +0300

    handle line chart with all component

--- chart/src/main/kotlin/lineChart/LineChart.kt
+++ chart/src/main/kotlin/lineChart/LineChart.kt
@@ -1,28 +1,15 @@
 package lineChart
 
-
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.*
-import androidx.compose.ui.text.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import lineChart.components.chartContainer
-import lineChart.lines.drawDefaultLineWithShadow
-import lineChart.lines.drawQuarticLineWithShadow
 import lineChart.model.BackGroundGrid
 import lineChart.model.LineParameters
-import lineChart.model.LineType
 
-@OptIn(ExperimentalTextApi::class)
 @Composable
 fun LineChart(
     modifier: Modifier = Modifier,
@@ -32,77 +19,26 @@ fun LineChart(
     showBackgroundGrid: BackGroundGrid = LineChartDefault.backGroundGrid,
     barWidthPx: Dp = LineChartDefault.backgroundLineWidth,
     animateChart: Boolean = LineChartDefault.ANIMATED_CHART,
-    pathEffect: PathEffect = LineChartDefault.pathEffect
+    pathEffect: PathEffect = LineChartDefault.pathEffect,
+    descriptionStyle: TextStyle = LineChartDefault.descriptionDefaultStyle
 ) {
-    val spacing = 100.dp
 
+    Column {
 
-    val textMeasure = rememberTextMeasurer()
-
-    val animatedProgress = remember {
-        if (animateChart) Animatable(0f) else Animatable(1f)
-    }
-    val upperValue = remember {
-        linesParameters.flatMap { it.data }.maxOrNull()?.plus(1.0) ?: 0.0
-    }
-    val lowerValue = remember {
-        linesParameters.flatMap { it.data }.minOrNull() ?: 0.0
-    }
-
-
-    Canvas(modifier = modifier.fillMaxSize().clipToBounds()) {
-        chartContainer(
-            xAxisData = xAxisData,
-            spacing = spacing,
-            textMeasure = textMeasure,
-            upperValue = upperValue.toFloat(),
-            lowerValue = lowerValue.toFloat(),
-            isShowBackgroundLines = showBackgroundGrid,
-            backgroundLineWidth = barWidthPx.toPx(),
-            backGroundLineColor = backGroundColor,
-            pathEffect = pathEffect,
+        ChartDescription(
+            chartLineDetails = linesParameters,
+            descriptionStyle = descriptionStyle
         )
 
-
-        val spaceBetweenXes = (size.width.toDp() - spacing) / xAxisData.size
-
-
-
-        linesParameters.forEach { line ->
-            if (line.lineType == LineType.DEFAULT_LINE) {
-
-                drawDefaultLineWithShadow(
-                    line = line,
-                    lowerValue = lowerValue.toFloat(),
-                    upperValue = upperValue.toFloat(),
-                    spacing = spacing,
-                    spaceBetweenXes = spaceBetweenXes,
-                    animatedProgress = animatedProgress,
-                    xAxisSize = xAxisData.size
-                )
-
-            } else {
-
-                drawQuarticLineWithShadow(
-                    line = line,
-                    lowerValue = lowerValue.toFloat(),
-                    upperValue = upperValue.toFloat(),
-                    spacing = spacing,
-                    spaceBetweenXes = spaceBetweenXes,
-                    animatedProgress = animatedProgress,
-                    xAxisSize = xAxisData.size
-                )
-
-            }
-        }
-    }
-
-
-    LaunchedEffect(animateChart) {
-        if (animateChart) {
-            animatedProgress.animateTo(
-                targetValue = 1f, animationSpec = tween(durationMillis = 1000, easing = LinearEasing)
-            )
-        }
+        ChartContent(
+            modifier = modifier,
+            linesParameters = linesParameters,
+            backGroundColor = backGroundColor,
+            xAxisData = xAxisData,
+            showBackgroundGrid = showBackgroundGrid,
+            barWidthPx = barWidthPx,
+            animateChart = animateChart,
+            pathEffect = pathEffect
+        )
     }
 }
