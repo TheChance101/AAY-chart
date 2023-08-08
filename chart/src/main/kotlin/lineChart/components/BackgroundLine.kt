59f1a7be5ae4ac6a5be9d40fcfd6584f5f6a8664
amnah.a <amnah44ali@gmail.com>
2023-08-08 19:02:18 +0300

    fix space between y axis

--- chart/src/main/kotlin/lineChart/components/BackgroundLine.kt
+++ chart/src/main/kotlin/lineChart/components/BackgroundLine.kt
@@ -27,10 +27,10 @@ fun DrawScope.backgroundLine(
     if (isShowBackgroundLines == BackGroundGrid.SHOW) {
         (0..5).forEach { i ->
             yAxisList.add(size.height - spacing.toPx() - i * size.height / 8f)
-            val yAlignmentValue = yAxisList[i] + 11.dp.toPx()
+            val yAlignmentValue = yAxisList[i] + 7.dp.toPx()
 
             // Ensure the line stays within the boundaries
-            val xStart = minX - 80
+            val xStart = minX - 24.dp.toPx()
             val xEnd = (size.width).coerceAtMost(xAxisMaxValue - spacing.toPx()).coerceAtLeast(minX)
 
             drawLine(
