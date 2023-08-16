0635977552bb9b0dc750c3b51dd003d15d711e46
AndrewAboalhana <hana_hany6@yahoo.com>
2023-08-17 00:15:27 +0300

    enhancement on worm chart to not work with negative

--- chart/src/commonMain/kotlin/com/aay/compose/wormChart/WormChart.kt
+++ chart/src/commonMain/kotlin/com/aay/compose/wormChart/WormChart.kt
@@ -2,7 +2,6 @@ package com.aay.compose.wormChart
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -12,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
+import com.aay.compose.utils.checkIfDataIsNegative
 import com.aay.compose.wormChart.components.wormCircle
 import com.aay.compose.wormChart.components.wormLine
 import kotlin.math.roundToInt
@@ -25,14 +25,16 @@ fun WormChart(
     val upperValue = remember { (data.maxOfOrNull { it }?.plus(1))?.roundToInt() ?: 0 }
     val lowerValue = remember { (data.minOfOrNull { it }?.toInt() ?: 0) }
 
+    checkIfDataIsNegative(data)
     Box (modifier = modifier.fillMaxSize().padding(16.dp)) {
         Canvas(
-            modifier = Modifier.aspectRatio(3/2f).align(Alignment.Center)
+            modifier = Modifier.fillMaxSize().align(Alignment.Center)
         ) {
-            val chartWidth = size.width.toDp() - 100.dp
+            val width = size.width.toDp()
+            val chartWidth = width - 100.dp
             val spacePerDataPoint = chartWidth / (data.size - 1)
             val centerY = (size.height / 2).toDp()
-            val lastX = size.width.toDp() - (100.dp + (data.size - 1) * spacePerDataPoint)
+            val lastX = width - (100.dp + (data.size - 1) * spacePerDataPoint)
             val lastY = centerY - ((data.last() - lowerValue) / (upperValue - lowerValue) * centerY)
 
             wormLine(data, centerY.toPx(), lowerValue, upperValue, lastX.toPx(), lastY.toPx(),color)
--- common/src/commonMain/kotlin/com/aay/common/WormChartSample.kt
+++ common/src/commonMain/kotlin/com/aay/common/WormChartSample.kt
@@ -6,6 +6,6 @@ import com.aay.compose.wormChart.WormChart
 @Composable
 fun WormChartSample(){
     WormChart(
-        listOf(20.0,50.0,10.0,20.0,0.0),
+        listOf(120.0,80.0,50.0,60.0,0.0),
     )
 }
