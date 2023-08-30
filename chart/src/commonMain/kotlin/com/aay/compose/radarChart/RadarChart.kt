afdac3af91ad30414a69d17fae4c1bd04fcfe74d
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-08-30 05:34:20 +0300

    move label measurement width impl to a separate function

--- chart/src/commonMain/kotlin/com/aay/compose/radarChart/RadarChart.kt
+++ chart/src/commonMain/kotlin/com/aay/compose/radarChart/RadarChart.kt
@@ -1,25 +1,19 @@
 package com.aay.compose.radarChart
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.aay.compose.radarChart.model.NetLinesStyle
 import com.aay.compose.radarChart.model.Polygon
 
@@ -41,15 +35,11 @@ fun RadarChart(
 
     Canvas(modifier = Modifier.fillMaxSize()) {
 
-        val labelWidth = textMeasurer.measure(
-            AnnotatedString(
-                text = radarLabels.maxByOrNull { it.length } ?: "",
-            ), style = labelsStyle
-        ).size.width.toDp().toPx()
+        val labelWidth = measureMaxLabelWidth(radarLabels, labelsStyle, textMeasurer)
         val radius = (size.minDimension / 2) - (labelWidth + 10.toDp().toPx())
         val labelRadius = (size.minDimension / 2) - (labelWidth / 2)
         val numLines = radarLabels.size
-        val radarChartConfig = getRadarConfig(labelRadius, radius, size, numLines, scalarSteps)
+        val radarChartConfig = calculateRadarConfig(labelRadius, radius, size, numLines, scalarSteps)
 
         drawRadarNet(netLinesStyle, radarChartConfig)
 
@@ -141,6 +131,18 @@ private fun DrawScope.drawPolygonShape(
     }
 }
 
+@OptIn(ExperimentalTextApi::class)
+private fun DrawScope.measureMaxLabelWidth(
+    radarLabels: List<String>,
+    labelsStyle: TextStyle,
+    textMeasurer: TextMeasurer
+): Float {
+    return textMeasurer.measure(
+        AnnotatedString(
+            text = radarLabels.maxByOrNull { it.length } ?: "",
+        ), style = labelsStyle
+    ).size.width.toDp().toPx()
+}
 
 
 
