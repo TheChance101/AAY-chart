081dbeb795c4d882c409afb32e246ba29a0b1648
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2023-09-04 02:32:35 +0300

    prepare gradle to publish chart

--- chart/build.gradle.kts
+++ chart/build.gradle.kts
@@ -1,12 +1,28 @@
-import org.jetbrains.compose.compose
-
 group = "com.aay"
 version = "1.0-SNAPSHOT"
+val pkgUrl = "https://github.com/TheChance101/AAY-chart"
+val gitUrl = "github.com:TheChance101/AAY-chart.git"
 
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
     id("com.android.library")
+    id("org.jetbrains.dokka") version "1.5.0"
+    id("maven-publish")
+    id("signing")
+}
+repositories {
+    google()
+    mavenCentral()
+}
+val dokkaOutputDir = "$buildDir/dokka"
+tasks.dokkaHtml {
+    outputDirectory.set(file(dokkaOutputDir))
+}
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaHtml)
 }
 
 kotlin {
@@ -57,5 +73,55 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+publishing {
+    repositories {
+        maven {
+            name = "Oss"
+            setUrl {
+                "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            }
+            credentials {
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+        maven {
+            name = "Snapshot"
+            setUrl { "https://s01.oss.sonatype.org/content/repositories/snapshots/" }
+            credentials {
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+    }
+
+    publications {
+        publications.configureEach {
+            if (this is MavenPublication) {
+                artifact(dokkaJar)
+                pom {
+                    name.set("AAY-Charts")
+                    description.set("Revenue Monster Kotlin Multiplatform SDK")
+                    url.set("https://github.com/TheChance101/AAY-chart")
+
+                    scm {
+                        connection.set("scm:git:git://$gitUrl")
+                        developerConnection.set("scm:git:ssh://$gitUrl")
+                        url.set(pkgUrl)
+                    }
+                }
+            }
+        }
+    }
+}
+if (System.getenv("GPG_PRIVATE_KEY") != null && System.getenv("GPG_PRIVATE_PASSWORD") != null) {
+    signing {
+        useInMemoryPgpKeys(
+            System.getenv("GPG_PRIVATE_KEY"),
+            System.getenv("GPG_PRIVATE_PASSWORD")
+        )
+        sign(publishing.publications)
     }
 }
