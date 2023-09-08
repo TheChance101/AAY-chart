61a546fd5ae2a57b7d7221cb26d4d0ce621c572a
AndrewAboalhana <hana_hany6@yahoo.com>
2023-09-08 17:59:51 +0300

    add ios dependency

--- chart/build.gradle.kts
+++ chart/build.gradle.kts
@@ -5,15 +5,33 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.dokka") version "1.5.0"
     id("convention.publication")
+    id ("org.jetbrains.kotlin.native.cocoapods") version "1.8.0"
 }
 group = "io.github.thechance101"
-version = "Beta-1.0.0"
+version = "Beta-0.0.1"
 
 kotlin {
-    android()
+    android {
+        publishLibraryVariants("release","debug")
+    }
     jvm("desktop") {
         jvmToolchain(11)
     }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    cocoapods {
+        version = "1.0.0"
+        summary = "Some description for the chart Module"
+        homepage = "Link to the chart Module homepage"
+        ios.deploymentTarget = "14.1"
+        podfile = project.file("../iosApp/Podfile")
+        framework {
+            baseName = "chart"
+            isStatic = true
+        }
+    }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -44,6 +62,20 @@ kotlin {
             }
         }
         val desktopTest by getting
+
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:2.3.3")
+            }
+        }
     }
 }
 
@@ -51,12 +83,23 @@ android {
     compileSdkVersion(34)
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
+        manifestPlaceholders["TheChance101"] = "io.github.thechance101"
         minSdkVersion(21)
         targetSdkVersion(34)
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+        create("staging") {
+            initWith(getByName("release"))
+            manifestPlaceholders["TheChance101"] = "io.github.thechance101"
+        }
     }
 }
 
